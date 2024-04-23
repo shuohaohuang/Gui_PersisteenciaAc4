60dc2d6613e4d8903ce9413c9bfa7f068f0c1b87
shuohaohuang <shuohao.huang.7e7@itb.cat>
2024-04-23 21:28:40 +0200

    a

--- Gui/obj/Debug/net8.0-windows/Gui.AssemblyInfo.cs
+++ Gui/obj/Debug/net8.0-windows/Gui.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df13997b9fdf4e0e182ffca47963c1c9fc05b0d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b15cda2d86bd73f98c0e4747e14dca767456aa0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
